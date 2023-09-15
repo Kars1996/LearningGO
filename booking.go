6aa46e35e626dc08bfda20697f99f7b51a18d0f7
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2023-09-15 17:18:01 +0100

    [+] FIles

---
+++ booking.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+funct main() {
+	fmt.Println("Welcome to a confence booking app")
+	fmt.Println("Get your tickets here")
+}
---
+++ calculator.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const n = 40000
+	fmt.Println(math.Sin(n))
+}
